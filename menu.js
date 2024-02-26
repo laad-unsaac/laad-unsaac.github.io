cc4a983822ebad8c1f1eda0ec5ec58edce03bb23
LAAD <laad@unsaac.edu.pe>
2024-02-26 11:02:34 -0500

    Laad

---
+++ menu.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('active'); // Agrega o quita la clase 'active' al menú al hacer clic en el menú desplegable
+}
